373b0dcd3e7628938d81ed5982b6f3f154372fc0
ravihoro <ravijohnhoro5@gmail.com>
2025-05-04 22:09:30 +0530

    Fix: responsive

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flame/game.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:snake_flame/direction.dart';
 import 'package:snake_flame/snake_game.dart';
@@ -9,58 +8,62 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isTablet(BuildContext context) =>
+        MediaQuery.of(context).size.shortestSide >= 600 &&
+        MediaQuery.of(context).size.shortestSide < 900;
+    bool isDesktop(BuildContext context) =>
+        MediaQuery.of(context).size.shortestSide >= 900;
+
     double screenWidth = MediaQuery.of(context).size.width;
 
-    double gameHeight = screenWidth * 0.5;
+    double gameHeight = screenWidth * 0.6;
     double gameWidth = gameHeight / (16 / 9);
 
-    double iconSize = kIsWeb ? screenWidth * 0.02 : 40;
+    double iconSize =
+        (isDesktop(context) || isTablet(context)) ? screenWidth * 0.04 : 40;
 
     final SnakeGame game = SnakeGame(context);
 
     final gameColumn = Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Expanded(
+        Flexible(
           flex: 8,
           child: GameWidget(game: game),
         ),
         SizedBox(height: 16),
-        Expanded(
-          flex: 2,
-          child: Column(
-            children: [
-              IconButton(
-                onPressed: () => game.updateDirection(Direction.up),
-                icon: Icon(
-                  Icons.keyboard_arrow_up,
-                  size: iconSize,
-                  color: Colors.green,
+        Column(
+          children: [
+            IconButton(
+              onPressed: () => game.updateDirection(Direction.up),
+              icon: Icon(
+                Icons.keyboard_arrow_up,
+                size: iconSize,
+                color: Colors.green,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.keyboard_arrow_left,
+                      size: iconSize, color: Colors.green),
+                  onPressed: () => game.updateDirection(Direction.left),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.keyboard_arrow_left,
-                        size: iconSize, color: Colors.green),
-                    onPressed: () => game.updateDirection(Direction.left),
-                  ),
-                  SizedBox(width: 50),
-                  IconButton(
-                    icon: Icon(Icons.keyboard_arrow_right,
-                        size: iconSize, color: Colors.green),
-                    onPressed: () => game.updateDirection(Direction.right),
-                  ),
-                ],
-              ),
-              IconButton(
-                icon: Icon(Icons.keyboard_arrow_down,
-                    size: iconSize, color: Colors.green),
-                onPressed: () => game.updateDirection(Direction.down),
-              ),
-            ],
-          ),
+                SizedBox(width: 50),
+                IconButton(
+                  icon: Icon(Icons.keyboard_arrow_right,
+                      size: iconSize, color: Colors.green),
+                  onPressed: () => game.updateDirection(Direction.right),
+                ),
+              ],
+            ),
+            IconButton(
+              icon: Icon(Icons.keyboard_arrow_down,
+                  size: iconSize, color: Colors.green),
+              onPressed: () => game.updateDirection(Direction.down),
+            ),
+          ],
         ),
         SizedBox(height: 16),
       ],
@@ -68,7 +71,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child: kIsWeb
+        child: isTablet(context) || isDesktop(context)
             ? SizedBox(
                 height: gameHeight,
                 width: gameWidth,
